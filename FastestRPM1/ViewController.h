1010b919943801afd7074c79df6186757fe1af1f
Kerry Toonen <kerrytoonen@gmail.com>
2016-01-21 11:07:56 -0500

    Initial Commit

---
+++ FastestRPM1/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  FastestRPM1
+//
+//  Created by Kerry Toonen on 2016-01-21.
+//  Copyright © 2016 Kerry Toonen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ FastestRPM1Tests/FastestRPM1Tests.m
@@ -0,0 +1,39 @@
+//
+//  FastestRPM1Tests.m
+//  FastestRPM1Tests
+//
+//  Created by Kerry Toonen on 2016-01-21.
+//  Copyright © 2016 Kerry Toonen. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface FastestRPM1Tests : XCTestCase
+
+@end
+
+@implementation FastestRPM1Tests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
